69ff58256a0de6e119ff3036122ab3bb2fc6029e
5win <94297900+5win@users.noreply.github.com>
2024-10-31 16:01:58 +0900

    [#37] fix: deploy.sh 수정
    - PID 취득 방식 lsof로 수정
    - jar파일 선택 방식 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ APP_NAME=gamsa
 cd $REPOSITORY
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f ${APP_NAME}-*.jar)
+CURRENT_PID=$(lsof -i :8080 -t)
 
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -18,7 +18,7 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | head -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
